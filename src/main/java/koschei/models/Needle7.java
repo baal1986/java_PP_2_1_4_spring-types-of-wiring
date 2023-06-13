bf678296ba2ace6ea3d7e71c71c9f7a69696bfa1
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-13 21:14:54 +0300

    refactor: classes Island2, Deth8, Needle7 Injected a dependency via a setter

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -8,8 +8,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
+    private Deth8 deth8;
+
+    @Autowired
+    public void setDeth8(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
+
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return deth8.toString();
     }
 }
